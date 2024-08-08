ef17a3276f16453193c1c5ef665dc19962e0c088
zhaowenzhe <zhaowenzhe@yushu.biz>
2024-08-08 15:29:44 +0800

    sql压缩上传

--- mysql-backup-tool/mysql-backup.sh
+++ mysql-backup-tool/mysql-backup.sh
@@ -11,10 +11,8 @@ mysql_tables=$7
 date_now=$(date +%Y%m%d-%H%M%S)
 echo "backup start"
 mkdir /backup-tools/bak
-mysqldump --host=$mysql_host -u$mysql_user -p$mysql_pwd  $mysql_database --no-create-db --set-gtid-purged=OFF --complete-insert --single-transaction --quick --no-autocommit --tables $( mysql -h$mysql_host -u$mysql_user -p$mysql_pwd  $mysql_database -Bse "$mysql_tables")| sed '/^USE/d'  > /backup-tools/bak/$env_name-$mysql_database-$backup_name-$date_now.sql && tar -zcvPf /backup-tools/bak/$env_name-$mysql_database-$backup_name-$date_now.tar /backup-tools/bak/$env_name-$mysql_database-$backup_name-$date_now.sql && ls -lhtr /backup-tools/bak/  
+mysqldump --host=$mysql_host -u$mysql_user -p$mysql_pwd  $mysql_database --no-create-db --set-gtid-purged=OFF --complete-insert --single-transaction --quick --no-autocommit --tables $( mysql -h$mysql_host -u$mysql_user -p$mysql_pwd  $mysql_database -Bse "$mysql_tables")| sed '/^USE/d'  > /backup-tools/bak/$env_name-$mysql_database-$backup_name-$date_now.sql && tar -zcvPf /backup-tools/bak/$env_name-$mysql_database-$backup_name-$date_now.tar /backup-tools/bak/$env_name-$mysql_database-$backup_name-$date_now.sql 
 echo "backup finish"
 echo "upload start"
 /backup-tools/coscli   cp /backup-tools/bak/$env_name-$mysql_database-$backup_name-$date_now.tar  cos://apm-static-resources-1307155645/mysql-backup/ &&
 echo "upload finish"
-ls /backup-tools/coscli_output/20240807*
-cat /backup-tools/coscli_output/20240807*/*
